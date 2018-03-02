9331cc6813de56854bf961429b261fa7502f4b6c
Jackiexiao <707610215@qq.com>
2018-03-02 15:53:59 +0800

    add gh-pages

--- tools/install_mtts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pip install jieba pypinyin
-sudo apt-get install libatlas3-base
-wget https://github.com/MontrealCorpusTools/Montreal-Forced-Aligner/releases/download/v1.0.0/montreal-forced-aligner_linux.tar.gz
-tar -zxvf montreal-forced-aligner_linux.tar.gz
